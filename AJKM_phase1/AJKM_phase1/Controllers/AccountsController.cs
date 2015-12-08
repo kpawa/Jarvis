8d94b4259f7d702f6f396a1ecab560451a31bbc4
hui202 <andrew.hui09@gmail.com>
2015-12-08 11:39:20 -0800

    added solution

---
+++ AJKM_phase1/AJKM_phase1/Controllers/AccountsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AJKM_phase1.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // leave blank
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // JALEN
+        public ActionResult LoginSignUp()
+        {
+            return View();
+        }
+        // JALEN
+        public ActionResult Login()
+        {
+            return View();
+        }
+        // JALEN
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        // MAT
+        public ActionResult ConsumerDashboard()
+        {
+            return View();
+        }
+        // MAT
+        public ActionResult AdminDashboard()
+        {
+            return View();
+        }
+        // MAT
+        public ActionResult BusinessDashboard()
+        {
+            return View();
+        }
+        // KULLY
+        public ActionResult Data()
+        {
+            return View();
+        }
+        // KULLY
+        public ActionResult Insights()
+        {
+            return View();
+        }
+        // KULLY
+        public ActionResult DeviceManager()
+        {
+            return View();
+        }
+    }
+}
---
+++ AJKM_phase1/AJKM_phase1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AJKM_phase1.MvcApplication" Language="C#" %>
