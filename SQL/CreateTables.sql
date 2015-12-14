cb668ed45fadd6a80871e467157639ca92aa3031
Matt Catellier <mattcatellier@gmail.com>
2015-12-14 12:29:41 -0800

    Matt updated SQL statements. Now have create table and 3 insert statements

--- SQL/CreateTables.sql
+++ SQL/CreateTables.sql
@@ -22,13 +22,14 @@ IS NOT NULL DROP TABLE Account;
 
 
 CREATE TABLE Account (
-	accountID	INT PRIMARY KEY,
-	email		VARCHAR(255),
-	password	VARCHAR(255),
-	firstname	VARCHAR(255),
-	lastname	VARCHAR(255),
-	birthdate	DATE,
-	type		VARCHAR(255)
+	accountID	INT IDENTITY(1000, 1) PRIMARY KEY,
+	email		VARCHAR(255) NOT NULL,
+	password	VARCHAR(255) NOT NULL,
+	firstname	VARCHAR(255) NOT NULL,
+	lastname	VARCHAR(255) NOT NULL,
+	birthdate	DATE NOT NULL,
+	type		VARCHAR(255) NOT NULL, 
+	CHECK (type IN ('Admin', 'Business', 'Consumer'))
 )
 
 CREATE TABLE Details (
@@ -43,31 +44,31 @@ CREATE TABLE Details (
 
 CREATE TABLE ProviderAccount (
 	accountID	INT PRIMARY KEY,
-	username	VARCHAR(255),
-	password	VARCHAR(255),
-	provider	VARCHAR(255)
+	username	VARCHAR(255) NOT NULL,
+	password	VARCHAR(255) NOT NULL,
+	provider	VARCHAR(255) NOT NULL, -- want to restrict somehow
 	FOREIGN KEY(accountID) REFERENCES Account(accountID)
 )
 
 CREATE TABLE DeviceData (
 	dataID		INT PRIMARY KEY,
-	state		VARCHAR(255),
-	kwhour		FLOAT(2) -- number with 2 decimals
+	state		VARCHAR(255) NOT NULL,
+	kwhour		FLOAT(2) NOT NULL -- number with 2 decimals
 )
 
 CREATE TABLE Device (
 	deviceID	INT PRIMARY KEY,
 	category	VARCHAR(255),
-	accountID	INT,
-	dataID		INT,
+	accountID	INT NOT NULL,
+	dataID		INT NOT NULL,
 	FOREIGN KEY(accountID) REFERENCES Account(accountID),
 	FOREIGN KEY(dataID) REFERENCES DeviceData(dataID)
 )
 
 CREATE TABLE StoredData (
 	deviceID	INT PRIMARY KEY,
-	state		VARCHAR(255),
-	timestamp	TIMESTAMP, -- stores both date and time
+	state		VARCHAR(255) NOT NULL,
+	datetime	DATETIME DEFAULT(getdate()), -- stores both date and time
 	FOREIGN KEY(deviceID) REFERENCES Device(deviceID)
 
 )
