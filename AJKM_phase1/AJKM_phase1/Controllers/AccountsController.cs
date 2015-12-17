0d409c692276b4efcd392b245828ea8678d98568
Matt Catellier <mattcatellier@gmail.com>
2015-12-17 14:08:29 -0800

    Added register devices page and FAQ's

--- AJKM_phase1/AJKM_phase1/Controllers/AccountsController.cs
+++ AJKM_phase1/AJKM_phase1/Controllers/AccountsController.cs
@@ -59,5 +59,10 @@ namespace AJKM_phase1.Controllers
             return View();
         }
 
+        public ActionResult RegisterDevices()
+        {
+            return View();
+        }
+
     }
 }
