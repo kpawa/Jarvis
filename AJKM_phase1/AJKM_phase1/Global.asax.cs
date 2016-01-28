3d0072685fa3ce32bab0f280a9f19f124db3769d
Andrew Hui <ahui27@bcit.ca>
2016-01-28 11:15:38 -0800

    Enabled CORS and added API

--- AJKM_phase1/AJKM_phase1/Global.asax.cs
+++ AJKM_phase1/AJKM_phase1/Global.asax.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using AJKM_phase1.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -11,6 +13,10 @@ namespace AJKM_phase1
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings
+            .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
