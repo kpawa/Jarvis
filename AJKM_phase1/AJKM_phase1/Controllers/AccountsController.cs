b8f9012e573aec30977514652bef8b432718d9b0
kpawa <kpawa3@my.bcit.ca>
2016-01-31 10:16:19 -0800

    Added Art Resources folder

--- AJKM_phase1/AJKM_phase1/Controllers/AccountsController.cs
+++ AJKM_phase1/AJKM_phase1/Controllers/AccountsController.cs
@@ -15,62 +15,49 @@ namespace AJKM_phase1.Controllers
         {
             return View();
         }
-        // JALEN
         public ActionResult LoginSignUp()
         {
             return View();
         }
-        // JALEN
         public ActionResult Login()
         {
             return View();
         }
-        // JALEN
         public ActionResult SignUp()
         {
             return View();
         }
-        // MAT
         public ActionResult ConsumerDashboard()
         {
             return View();
         }
-        // MAT
-        public ActionResult AdminDashboard()
-        {
-            return View();
-        }
-        // KULLY
-        public ActionResult Data()
-        {
-            return View();
-        }
-        // KULLY
-        public ActionResult Insights()
-        {
-            return View();
-        }
-        // KULLY
-        public ActionResult DeviceManager()
-        {
-            return View();
-        }
-
-        public ActionResult RegisterDevices()
-        {
-            return View();
-        }
-
         public ActionResult AccountView()
         {
             return View();
         }
-
+        public ActionResult Data()
+        {
+            return View();
+        }
+        public ActionResult Insights()
+        {
+            return View();
+        }
+        public ActionResult DeviceManager()
+        {
+            return View();
+        }
+        public ActionResult RegisterDevices()
+        {
+            return View();
+        }
+        public ActionResult AdminDashboard()
+        {
+            return View();
+        }
         public ActionResult ViewAllConsumerAccounts()
         {
             return View();
         }
-
-
     }
 }
