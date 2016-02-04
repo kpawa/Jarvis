4d715bad73e08872d4796b275750f6d12e9ce173
Andrew Hui <ahui27@bcit.ca>
2016-02-04 11:42:19 -0800

    Finished setting up authorization. Added Login/Register views.

--- AJKM_phase1/AJKM_phase1/Global.asax.cs
+++ AJKM_phase1/AJKM_phase1/Global.asax.cs
@@ -1,7 +1,10 @@
 ﻿using AJKM_phase1.App_Start;
+using AJKM_phase1.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
+using System.Threading;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Http;
@@ -23,5 +26,24 @@ namespace AJKM_phase1
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
+        void Application_PostAuthenticateRequest()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var name = User.Identity.Name; // Get current user name.
+
+                SecurityEntities context = new SecurityEntities();
+                var user = context.AspNetUsers.Where(u => u.UserName == name).FirstOrDefault();
+                IQueryable<string> roleQuery = from u in context.AspNetUsers
+                                               from r in u.AspNetRoles
+                                               where u.UserName == Context.User.Identity.Name
+                                               select r.Name;
+                string[] roles = roleQuery.ToArray();
+
+                HttpContext.Current.User = Thread.CurrentPrincipal =
+                                           new GenericPrincipal(User.Identity, roles);
+            }
+        }
+
     }
 }
