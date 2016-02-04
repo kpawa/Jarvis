137c9525e87b20c7471584c0653fb775e3759c61
Andrew Hui <ahui27@bcit.ca>
2016-02-04 12:54:36 -0800

    added lockout security feature

--- AJKM_phase1/AJKM_phase1/Controllers/AccountsController.cs
+++ AJKM_phase1/AJKM_phase1/Controllers/AccountsController.cs
@@ -39,7 +39,7 @@ namespace AJKM_phase1.Controllers
 
             if (ModelState.IsValid)
             {
-                if (identityUser != null)
+                if (ValidLogin(login))
                 {
                     IAuthenticationManager authenticationManager
                                            = HttpContext.GetOwinContext().Authentication;
@@ -70,7 +70,13 @@ namespace AJKM_phase1.Controllers
         public ActionResult Register(RegisteredUser newUser)
         {
             var userStore = new UserStore<IdentityUser>();
-            var manager = new UserManager<IdentityUser>(userStore);
+            UserManager<IdentityUser> manager = new UserManager<IdentityUser>(userStore)
+            {
+                UserLockoutEnabledByDefault = true,
+                DefaultAccountLockoutTimeSpan = new TimeSpan(0, 10, 0),
+                MaxFailedAccessAttemptsBeforeLockout = 3
+            };
+
             var identityUser = new IdentityUser()
             {
                 UserName = newUser.UserName,
@@ -97,6 +103,46 @@ namespace AJKM_phase1.Controllers
             authenticationManager.SignOut();
             return RedirectToAction("Index", "Home");
         }
+        bool ValidLogin(Login login)
+        {
+            UserStore<IdentityUser> userStore = new UserStore<IdentityUser>();
+            UserManager<IdentityUser> userManager = new UserManager<IdentityUser>(userStore)
+            {
+                UserLockoutEnabledByDefault = true,
+                DefaultAccountLockoutTimeSpan = new TimeSpan(0, 10, 0),
+                MaxFailedAccessAttemptsBeforeLockout = 3
+            };
+            var user = userManager.FindByName(login.UserName);
+
+            if (user == null)
+                return false;
+
+            // User is locked out.
+            if (userManager.SupportsUserLockout && userManager.IsLockedOut(user.Id))
+                return false;
+
+            // Validated user was locked out but now can be reset.
+            if (userManager.CheckPassword(user, login.Password))
+            {
+                if (userManager.SupportsUserLockout
+                 && userManager.GetAccessFailedCount(user.Id) > 0)
+                {
+                    userManager.ResetAccessFailedCount(user.Id);
+                }
+            }
+            // Login is invalid so increment failed attempts.
+            else {
+                bool lockoutEnabled = userManager.GetLockoutEnabled(user.Id);
+                if (userManager.SupportsUserLockout && userManager.GetLockoutEnabled(user.Id))
+                {
+                    userManager.AccessFailed(user.Id);
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
         /* ============================ */
         /* ===== ADMIN PRIVILEGES ===== */
         /* ============================ */
