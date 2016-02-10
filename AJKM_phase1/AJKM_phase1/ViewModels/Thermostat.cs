e0db6e300f43cb432818e778945ddaaac283f914
A00970788 <jtkenny21@gmail.com>
2016-02-10 13:49:07 -0800

    Added thermostat model & repo

---
+++ AJKM_phase1/AJKM_phase1/ViewModels/Thermostat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AJKM_phase1.ViewModels
+{
+    public class Thermostat
+    {
+        public string Current_Temperature { get; set; }
+        public string Target_Temperature { get; set; }
+        public string Name_Long { get; set; }
+        public string Device_Id { get; set; }
+        public Thermostat()
+        {
+
+        }
+        public Thermostat(string tempeture)
+        {
+            this.Current_Temperature = tempeture;
+        }
+        public Thermostat(string name, string tempeture)
+        {
+            this.Current_Temperature = tempeture;
+            this.Name_Long = name;
+        }
+
+        public Thermostat(string device_id, string name, string curr_temp, string tar_temp)
+        {
+            this.Device_Id = device_id;
+            this.Current_Temperature = curr_temp;
+            this.Target_Temperature = tar_temp;
+            this.Name_Long = name;
+        }
+        public override string ToString()
+        {
+            return "Device Id: " + this.Device_Id + ", Name: " + this.Name_Long + ", Current Temperature: " + this.Current_Temperature + ", Target temperature: " + this.Target_Temperature;
+        }
+    }
+}
