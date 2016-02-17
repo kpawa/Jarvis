4b561dfdea1ef5ec56db3e1ddbbfd6d93bb4c884
Matt Catellier <mattcatellier@gmail.com>
2016-02-17 08:57:21 -0800

    Changed the name of the somke alarm repo.

--- AJKM_phase1/AJKM_phase1/Models/SmokeCoAlarmVMRepo.cs
+++ AJKM_phase1/AJKM_phase1/Models/SmokeCoAlarmVMRepo.cs
@@ -9,8 +9,6 @@ namespace AJKM_phase1.Models
     public class SmokeCoAlarmVMRepo
     {
         const string ACCESS_TOKEN = "c.QY4JkcdwELewWkIDfbgCm2WSEHlaKSvI6g6dpWVOf7levs96rMRByP4xRQksCJUfxrSgYKPwiUKzj1OcgIad2nxerddqp4QvMleuC55br637xaGnychVSl4yMUoQBoWI8uFg1dI9uiK2hZ49";
-
-
         // WILLR RETURN A LIST OF ALL THERMOSTATS WITH THIER NAME, STATE etc...
         public async Task<IEnumerable<SmokeCoAlarmVM>> GetCameras()
         {
